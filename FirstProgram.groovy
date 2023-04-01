8aa27a61b4d8de2f1b438840f92a60539fffcb40
NIKHIL-066 <nikhil.hegde.08@gmail.com>
2023-04-01 19:56:43 +0530

    First Program

---
+++ FirstProgram.groovy
@@ -0,0 +1,12 @@
+class FirstProgram{
+    static void main(args){
+        // Single Line Comment
+        /* 
+            Multiline
+            comment
+        */
+        print("The string does not end with new line -- ")
+        println("The string ends with line")
+        print("Arguments passed(if any): "+args)
+    }
+}
